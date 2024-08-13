880fe6ab95da6e6736cf4d02b22b8f59598bfed6
canishmax <canishmax@gmail.com>
2024-08-13 08:31:14 +0000

    change design

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-r from-blue-500 to-purple-500 h-screen flex flex-col justify-center items-center">
+    <div className="h-screen flex flex-col justify-center items-center bg-blue-900">
       <div className="text-center">
-        <h1 className="text-4xl md:text-6xl font-bold text-white bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-yellow-500">
+        <h1 className="text-4xl md:text-6xl font-bold text-white bg-clip-text text-transparent bg-gradient-to-r from-yellow-300 via-red-400 to-pink-500">
           Share your thoughts and connect with others on OwnThoughts
         </h1>
         <p className="text-lg md:text-xl text-white mt-4">
-          Here we unleash our thoughts and share with others with no identity. No one will judge you.
+          Here, we unleash our thoughts and share them with others in an <span className="text-2xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">anonymous</span> and judgment-free space.
         </p>
       </div>
     </div>
