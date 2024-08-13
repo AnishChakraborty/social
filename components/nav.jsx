377ee4b3496afb835dab5cb3259e9d1aff2745c3
canishmax <canishmax@gmail.com>
2024-08-13 10:23:05 +0000

    changes in nav

--- components/nav.jsx
+++ components/nav.jsx
@@ -4,9 +4,9 @@ import React, { useEffect, useState } from 'react'
 
 export const Nav = () => {
   return (
-    <div className='h-14 w-full bg-black flex'>
+    <div className='h-16 w-full bg-black flex'>
         <Link href="/">
-        <div className='h-10 w-10 bg-white rounded-full mx-5 my-2'></div>
+        <div className='h-10 w-10 bg-white rounded-full mx-5 my-3'></div>
         </Link>
     </div>
   )
